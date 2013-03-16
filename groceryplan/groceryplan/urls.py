c0df3c5e45d2873a0f1859928566119a419c74e2
Jeff <jeff@hnt0316.wlan.wharton.upenn.edu>
2013-03-16 13:19:39 -0400

    Initial views and urls.

--- groceryplan/groceryplan/urls.py
+++ groceryplan/groceryplan/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'prices.views.index'),  
+    url(r'^product/(?P<product_id>\d+)/$', 'prices.views.product'),
+    url(r'^admin/', include(admin.site.urls)),
 )
--- groceryplan/prices/views.py
+++ groceryplan/prices/views.py
@@ -1,1 +1,33 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from prices.models import ProductType, Product
+from django.shortcuts import render
+
+
+def index(request):
+    ptype = ProductType.objects.all()
+    p = Product.objects.all()    
+    latest_poll_list = ProductType.objects.all().order_by('-name')[:5]    
+    t = loader.get_template('product/index.html')
+
+    context = {
+        'product_type': ptype,
+        'product': p,
+        'latest_poll_list': latest_poll_list     
+    }   
+
+    return render(request, 'index.html', context)    
+
+def product(request, product_id):
+    ptype = ProductType.objects.all()
+    p = Product.objects.all()    
+    latest_poll_list = ProductType.objects.all().order_by('-name')[:5]    
+    t = loader.get_template('product/index.html')
+
+    context = {
+        'product_type': ptype,
+        'product': p,
+        'latest_poll_list': latest_poll_list     
+    }   
+
+    return render(request, 'index.html', context)    
