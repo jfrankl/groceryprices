862d5e378dbd950d007904dc23b89e9f868a8209
Jeff <jeff@hnt0316.wlan.wharton.upenn.edu>
2013-03-16 12:54:05 -0400

    Add admin.

---
+++ groceryplan/prices/admin.py
@@ -0,0 +1,7 @@
+from prices.models import Store, Product, Feature, ProductType
+from django.contrib import admin
+
+admin.site.register(Store)
+admin.site.register(Product)
+admin.site.register(Feature)
+admin.site.register(ProductType)
