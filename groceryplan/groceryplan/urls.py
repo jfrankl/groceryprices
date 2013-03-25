207c16aca4a23931c2274442316ee06efa75a3d5
Jeff <jfinbox@comcast.net>
2013-03-25 11:06:51 -0400

    Switch function-based generic views to class-based generic views.

--- groceryplan/groceryplan/urls.py
+++ groceryplan/groceryplan/urls.py
@@ -4,16 +4,7 @@ from prices.views import ProductListView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'groceryplan.views.home', name='home'),
-    # url(r'^groceryplan/', include('groceryplan.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^$', 'prices.views.index'),  
-    url(r'^food/(?P<slug>(.+))/$', 'prices.views.products_by_food'),
-    url(r'^sood/(?P<slug>(.+))/$', 'ProductListView.as_view()'),    
+    url(r'^$', 'prices.views.index'),
+    url(r'^food/(?P<slug>(.+))/$', ProductListView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
--- groceryplan/prices/views.py
+++ groceryplan/prices/views.py
@@ -1,28 +1,7 @@
-from django.template import Context, loader
-from django.http import HttpResponse
 from prices.models import Food, Product
-from django.shortcuts import render, render_to_response
-from django.http import Http404
-from django.views.generic import list_detail
+from django.shortcuts import render_to_response
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404
-
-
-class ProductListView(ListView):
-
-    context_object_name = "Product_list",
-    template_name = "food/index.html",
-
-    def get_queryset(self):
-        self.food = get_object_or_404(Food, name__iexact=self.args[0])
-        return Product.objects.filter(publisher=self.food)
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super(ProductListView, self).get_context_data(**kwargs)
-        # Add in the publisher
-        context['food'] = self.food
-        return context
 
 
 def index(request):
@@ -30,22 +9,21 @@ def index(request):
 
     context = {
         'product_type': ptype
-    }   
+    }
 
-    return render_to_response('index.html', context)    
+    return render_to_response('index.html', context)
 
 
-def products_by_food(request, slug):
+class ProductListView(ListView):
 
-    # Look up the publisher (and raise a 404 if it can't be found).
-   
-    food_var = get_object_or_404(Food, slug=slug)
+    context_object_name = "Product_list"
+    template_name = "food/index.html"
 
-    # Use the object_list view for the heavy lifting.
-    return list_detail.object_list(
-        request,
-        queryset = Product.objects.filter(name=food_var),
-        template_name = "food/index.html",
-        template_object_name = "Product",
-        extra_context = {"Header" : food_var.name}
-    )
+    def get_queryset(self):
+        self.food = get_object_or_404(Food, slug=self.kwargs['slug'])
+        return Product.objects.filter(name=self.food)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['food'] = self.food
+        return context
