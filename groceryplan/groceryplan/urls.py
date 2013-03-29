fafda7a94ed0be5514fbf39648b1a839586365f5
Jeff Frankl <jfinbox@comcast.net>
2013-03-29 13:49:42 -0400

    Remove trailing whitespace.

--- groceryplan/groceryplan/urls.py
+++ groceryplan/groceryplan/urls.py
@@ -4,7 +4,8 @@ from prices.views import ProductListView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'prices.views.index'),
-    url(r'^food/(?P<slug>(.+))/$', ProductListView.as_view()),
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^$', 'prices.views.index'),
+                       url(r'^food/(?P<slug>(.+))/$',
+                           ProductListView.as_view()),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
