be4ac5877453cf6349026a18495758b280d80814
Jeff Frankl <jfinbox@comcast.net>
2013-04-09 19:17:18 -0400

    Change index to class-based view.

--- groceryplan/groceryplan/urls.py
+++ groceryplan/groceryplan/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from prices.views import ProductListView
+from prices.views import ProductListView, Index
 from tastypie.api import Api
 from prices.api.resources import ProductResource, FoodResource
 
@@ -11,7 +11,7 @@ v1_api.register(ProductResource())
 v1_api.register(FoodResource())
 
 urlpatterns = patterns('',
-                       url(r'^$', 'prices.views.index'),
+                       url(r'^$', Index.as_view()),
                        url(r'^api/', include(v1_api.urls)),
                        url(r'^food/(?P<slug>(.+))/$',
                            ProductListView.as_view()),
