4344660e6dbed2f7083bfa6e8e3f9483b91bbd3e
Jeff Frankl <jfinbox@comcast.net>
2013-04-15 03:57:44 -0400

    Add slug auto-generation for store.

--- groceryplan/prices/admin.py
+++ groceryplan/prices/admin.py
@@ -5,7 +5,11 @@ from django.contrib import admin
 class FoodAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
-admin.site.register(Store)
+
+class StoreAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Store, StoreAdmin)
 admin.site.register(Product)
 admin.site.register(Feature)
 admin.site.register(Food, FoodAdmin)
